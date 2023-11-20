402d74077339a61612984d921aabb32603b52928
Viktor Oshkalo <vvicmail@gmail.com>
2023-11-20 15:43:13 +0200

    HW7 task 1 fix comments

--- HW7/task1/main.go
+++ HW7/task1/main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
-func printAverage(channelAvg chan float32) {
+func printAverage(channelAvg chan float32, channelDone chan bool) {
 	fmt.Println("printAvg started")
 	avg := <-channelAvg
 	fmt.Println("Average value is: ", avg)
+	channelDone <- true
 }
 
-func calcAverage(channelIn chan int, channelOut chan float32) {
+func calcAverage(channelNumbers chan int, channelAvg chan float32) {
 	fmt.Println("calcAverage started")
 	var sum int
 	var counter int
-	for val := range channelIn {
+	for val := range channelNumbers {
 		fmt.Println("recieving from numbers channel: ", val)
 		sum += val
 		counter++
@@ -24,8 +24,8 @@ func calcAverage(channelIn chan int, channelOut chan float32) {
 
 	avg := float32(sum) / float32(counter)
 	fmt.Println("sending average value into channel: ", avg)
-	channelOut <- avg
-	close(channelOut)
+	channelAvg <- avg
+	close(channelAvg)
 }
 
 func generateNumbers(numbersCount int, maxNumber int, channelNumbers chan int) {
@@ -46,11 +46,11 @@ func main() {
 
 	channelNumbers := make(chan int, numbersCount)
 	channelAvg := make(chan float32)
-
+	channelDone := make(chan bool)
 	go generateNumbers(numbersCount, maxValue, channelNumbers)
 	go calcAverage(channelNumbers, channelAvg)
-	go printAverage(channelAvg)
+	go printAverage(channelAvg, channelDone)
 
-	time.Sleep(time.Second * 2)
+	<-channelDone
 	fmt.Println("Exit")
 }

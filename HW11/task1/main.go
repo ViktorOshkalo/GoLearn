d5eb92e7f2ec3caecb5357cd1abac0f7be0d2e2d
viktoroshkalo <vvicmail@gmail.com>
2023-12-03 00:25:38 +0200

    Improvements

--- HW11/task1/main.go
+++ HW11/task1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"regexp"
@@ -9,49 +10,76 @@ import (
 func main() {
 	fmt.Println("Hello!")
 
-	text, err := os.ReadFile("numbers.txt")
+	reader, err := ReadFileByLineBatches("numbers.txt", 3)
 	if err != nil {
-		panic("unable to read file")
+		panic(err)
 	}
-
-	fmt.Println("Input text:")
-	fmt.Println(string(text))
 
 	rexp := regexp.MustCompile(`(\+?(?P<code_country>\d{2})[. -]?)?\(?(?P<code_operator>\d{3})\)?[. -]?(?P<number_p1>\d{3})[. -]?(?P<number_p2>\d{2})[. -]?(?P<number_p3>\d{2})`)
 
-	matches := rexp.FindAllStringSubmatch(string(text), -1)
+	for text := range reader {
 
-	fmt.Println("\nAll findings:")
-	for _, match := range matches {
-		fmt.Println(match[0])
-	}
+		fmt.Println("\nText batch received: ")
+		fmt.Println(text)
 
-	var numberInfos []map[string]string
-	for _, match := range matches {
-		subMatchMap := make(map[string]string)
-		for i, name := range rexp.SubexpNames() {
-			if i != 0 && name != "" {
-				subMatchMap[name] = string(match[i])
+		matches := rexp.FindAllStringSubmatch(string(text), -1)
+
+		fmt.Println("\nAll findings:")
+		for _, match := range matches {
+			fmt.Println(match[0])
+		}
+
+		var numberInfos []map[string]string
+		for _, match := range matches {
+			subMatchMap := make(map[string]string)
+			for i, name := range rexp.SubexpNames() {
+				if i != 0 && name != "" {
+					subMatchMap[name] = string(match[i])
+				}
+			}
+			numberInfos = append(numberInfos, subMatchMap)
+		}
+
+		fmt.Println("\nNumbers normalized:")
+		for _, numberInfo := range numberInfos {
+			if numberInfo["code_country"] != "" {
+				fmt.Printf("+%s (%s) %s-%s\n",
+					numberInfo["code_country"],
+					numberInfo["code_operator"],
+					numberInfo["number_p1"],
+					numberInfo["number_p2"]+numberInfo["number_p3"])
+			} else {
+				fmt.Printf("(%s) %s-%s\n",
+					numberInfo["code_operator"],
+					numberInfo["number_p1"],
+					numberInfo["number_p2"]+numberInfo["number_p3"])
 			}
 		}
-		numberInfos = append(numberInfos, subMatchMap)
 	}
-
-	fmt.Println("\nNumbers normalized:")
-	for _, numberInfo := range numberInfos {
-		if numberInfo["code_country"] != "" {
-			fmt.Printf("+%s (%s) %s-%s\n",
-				numberInfo["code_country"],
-				numberInfo["code_operator"],
-				numberInfo["number_p1"],
-				numberInfo["number_p2"]+numberInfo["number_p3"])
-		} else {
-			fmt.Printf("(%s) %s-%s\n",
-				numberInfo["code_operator"],
-				numberInfo["number_p1"],
-				numberInfo["number_p2"]+numberInfo["number_p3"])
-		}
-	}
-
 	fmt.Println("Exit")
+}
+
+// lets say file is super big
+func ReadFileByLineBatches(fileName string, linesBatchSize int) (<-chan string, error) {
+	out := make(chan string)
+	file, err := os.Open(fileName)
+	if err != nil {
+		return out, nil
+	}
+
+	go func() {
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			linesOut := scanner.Text()
+			for i := 1; i < linesBatchSize && scanner.Scan(); i++ {
+				linesOut += "\n" + scanner.Text()
+			}
+			out <- linesOut
+		}
+		close(out)
+	}()
+
+	return out, nil
 }

16e14906d3f465ab4d686b9a4a83b07249f615dd
Viktor Oshkalo <vvicmail@gmail.com>
2024-01-07 19:00:39 +0200

    Fixes to HW12

--- HW12/main.go
+++ HW12/main.go
@@ -7,131 +7,152 @@ import (
 	"strings"
 )
 
-// strategy
-type ITextProcessingStrategy interface {
-	Process(text string) string
+// text processor interface
+type ITextProcessor interface {
+	Process(text string) map[string]int
 }
 
-type TextReplacerStrategy struct {
+// strtegies
+type WordsCounter struct{}
+
+func (wc WordsCounter) Process(text string) map[string]int {
+	fmt.Printf("\nIncoming text:\n\n%s", text)
+	words := strings.Fields(text)
+
+	out := map[string]int{
+		"words count": len(words),
+	}
+	return out
+}
+
+type WordsEntriesCounter struct{}
+
+func (wc WordsEntriesCounter) Process(text string) map[string]int {
+	fmt.Printf("\nIncoming text:\n\n%s", text)
+
+	re := regexp.MustCompile(`\w+`)
+	matchWords := re.FindAllString(text, -1)
+
+	stats := make(map[string]int)
+	for _, word := range matchWords {
+		stats[word]++
+	}
+
+	return stats
+}
+
+// decorators
+// space cleaner
+type SpaceCleaner struct {
+	base ITextProcessor
+}
+
+func (sr SpaceCleaner) Process(text string) map[string]int {
+	re := regexp.MustCompile(`\s+`)
+	text = re.ReplaceAllString(text, " ")
+	if sr.base != nil {
+		return sr.base.Process(text)
+	} else {
+		panic("decorator base is not set")
+	}
+}
+
+// word replacer
+type WordReplacer struct {
+	base ITextProcessor
 	From string
 	To   string
 }
 
-func (r TextReplacerStrategy) Process(text string) string {
-	return strings.ReplaceAll(text, r.From, r.To)
-}
-
-func (r *TextReplacerStrategy) InitUserParams() {
-	var oldWord, newWord string
-	fmt.Println("Enter old word: ")
-	fmt.Scanln(&oldWord)
-	fmt.Println("Enter new word: ")
-	fmt.Scanln(&newWord)
-	r.From = oldWord
-	r.To = newWord
-}
-
-type SpaceCleanerStrategy struct {
-	SelectorRegex string
-	To            string
-}
-
-func (r SpaceCleanerStrategy) Process(text string) string {
-	re := regexp.MustCompile(r.SelectorRegex)
-	return re.ReplaceAllString(text, r.To)
-}
-
-func (r *SpaceCleanerStrategy) InitParams() {
-	r.SelectorRegex = `\s+`
-	r.To = " "
-}
-
-// text processor
-type ITextProcessor interface {
-	ProcessText(text string) string
-}
-
-// some text processor implementation
-type TextProcessor struct {
-}
-
-func (p TextProcessor) ProcessText(text string) string {
-	// doing something
-	return text
-}
-
-// decorator
-type TextProcessorDecorator struct {
-	base     ITextProcessor
-	strategy ITextProcessingStrategy // decorator processing logic moved to strategy
-}
-
-func (p TextProcessorDecorator) ProcessText(text string) string {
-	if p.base == nil {
-		panic("processor base is not set")
-	}
-	text = p.base.ProcessText(text)
-	return p.strategy.Process(text)
-}
-
-func GetNextTextProcessor(command string, baseProcessor ITextProcessor) (ITextProcessor, error) {
-	switch command {
-	case "space":
-		strategy := SpaceCleanerStrategy{}
-		strategy.InitParams()
-		return TextProcessorDecorator{base: baseProcessor, strategy: &strategy}, nil
-	case "word":
-		strategy := TextReplacerStrategy{}
-		strategy.InitUserParams()
-		return TextProcessorDecorator{base: baseProcessor, strategy: &strategy}, nil
-	default:
-		return nil, fmt.Errorf("unknown command: %s", command)
+func (r WordReplacer) Process(text string) map[string]int {
+	text = strings.ReplaceAll(text, r.From, r.To)
+	if r.base != nil {
+		return r.base.Process(text)
+	} else {
+		panic("decorator base is not set")
 	}
 }
 
-var commands map[string]string = map[string]string{
-	"space": "add space cleaner",
-	"word":  "add word replacer",
-	"done":  "start text modifying",
-}
-
-func printCommands() {
-	fmt.Println("Available commands:")
-	for key, value := range commands {
-		fmt.Printf("%s - %s\n", key, value)
-	}
-}
-
+// main
 func main() {
-	fmt.Println("Hello")
-
 	var text = ReadFile("file.txt")
 	fmt.Println("Original Text:")
 	fmt.Println(text)
 
-	fmt.Printf("\nEnter a commands to build text processor.\n")
-	printCommands()
+	// test case
+	fmt.Println("\n\nTest case mode")
+	processor := SpaceCleaner{base: WordReplacer{From: "sun", To: "SUN", base: WordsEntriesCounter{}}}
+	res := processor.Process(text)
+	PrintResult(res)
 
-	var processor ITextProcessor = TextProcessor{}
+	// user input
+	fmt.Println("\n\nUser input mode")
+	strategy := GetStrategy()
+	userProcessor := GetDecorator(strategy)
+	res = userProcessor.Process(text)
+	PrintResult(res)
+}
+
+// helper methods
+func GetStrategy() ITextProcessor {
+	fmt.Println("Strategy setup")
+	fmt.Println("Available strategies:")
+	fmt.Println("word: Calculate words count")
+	fmt.Println("entry: Calculate words entries count")
+
 	for {
-		fmt.Println("\nEnter command: ")
 		var command string
+		fmt.Print("Enter a command: ")
 		fmt.Scanln(&command)
 
-		if command == "done" {
-			text = processor.ProcessText(text)
-			fmt.Println("Modified text:")
-			fmt.Println(text)
-			return
-		}
-
-		processorNext, err := GetNextTextProcessor(command, processor)
-		if err != nil {
-			fmt.Println(err)
+		switch command {
+		case "word":
+			return WordsCounter{}
+		case "entry":
+			return WordsEntriesCounter{}
+		default:
+			fmt.Println("Strategy not exists. Enter correct command")
 			continue
 		}
+	}
+}
 
-		processor = processorNext
+func GetDecorator(base ITextProcessor) ITextProcessor {
+	fmt.Println("Decorators setup")
+	fmt.Println("Available decorators:")
+	fmt.Println("space: Space remover")
+	fmt.Println("word: Word replacer")
+	fmt.Println("done: Exit decorators setup")
+
+	var processor ITextProcessor = base
+	for {
+		var command string
+		fmt.Print("Enter a command: ")
+		fmt.Scanln(&command)
+
+		switch command {
+		case "space":
+			processor = SpaceCleaner{base: processor}
+		case "word":
+			var oldWord, newWord string
+			fmt.Println("Enter old word: ")
+			fmt.Scanln(&oldWord)
+			fmt.Println("Enter new word: ")
+			fmt.Scanln(&newWord)
+			processor = WordReplacer{base: processor, From: oldWord, To: newWord}
+		case "done":
+			return processor
+		default:
+			fmt.Println("Wrong command")
+			continue
+		}
+	}
+}
+
+func PrintResult(res map[string]int) {
+	fmt.Println("\n\nResult:")
+	for k, v := range res {
+		fmt.Printf("%s: %d\n", k, v)
 	}
 }
 

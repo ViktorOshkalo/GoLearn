4076e02be7ed9524162486de88dd55952625f3cf
Viktor Oshkalo <vvicmail@gmail.com>
2024-01-23 22:10:05 +0200

    course work folder added

---
+++ CourseWork/configuration/configuration.go
@@ -0,0 +1,7 @@
+package configuration
+
+var ConnectionString string = "user:password@tcp(localhost:3306)/ProductsAppDb?parseTime=true"
+var SqlProvider string = "mysql"
+
+var User string = "admin"
+var Password string = "admin"
